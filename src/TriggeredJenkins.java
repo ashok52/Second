c94744748d6dafc702ba4a7819a6178169cf9ff3
Chitranjan Bharti <chitranjan.bharti@vertisinfotech.com>
2017-05-03 16:32:28 +0530

    first commit

---
+++ src/TriggeredJenkins.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Chitranjan on 5/3/2017.
+ */
+public class TriggeredJenkins {
+
+    public static void main(String[] args){
+        System.out.println("Run Success");
+    }
+
+
+}
